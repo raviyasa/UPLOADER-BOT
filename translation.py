7254e6ec9d7f2c641a4711d1afdbdc2882a79cd6
Ravidu Yasas <88565232+raviyasa@users.noreply.github.com>
2021-11-03 18:07:15 +0845

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,17 @@
 class Translation(object):
     START_TEXT = """Hi {},
-I'm URL X Uploader!
+I'm URL Uploader!
 You can upload direct link, Using this bot!😊
 
 /help for more details!
 
-Made by @darkz_hacker_devil
-Powered by @media_bot_updates"""
+©️ Made by <a href="https://t.me/darkz_hacker_devil">Ravidu Yasas</a>
+©️ Powered by <a href="https://t.me/media_bot_updates">Bot Shadows</a>"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "Downloading 📥📥..."
-    UPLOAD_START = "Uploading 📤📤..."
+    DOWNLOAD_START = "Downloading 📥..."
+    UPLOAD_START = "Uploading 📤..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @crazy_url_upload_bot\n\n<b>Join : @media_bot_updates</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@crazy_url_upload_bot"
